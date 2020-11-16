2f646ecacbd4607acfd9ec74127879cb0e51e846
Nick Hallman <Nick.Hallman@D2L.com>
2020-11-16 16:25:19 -0500

    comments

--- model/stateHistory.js
+++ model/stateHistory.js
@@ -1,21 +1,21 @@
+// TODO: expand to support redo
 export default class StateHistory {
 
 	past = [];
 	isUndoing = false;
 
 	save(state, undo) {
+		// if undoing causes a state change
+		// to get saved we need to ignore it
 		if (!this.isUndoing) {
-			console.log(state, undo);
 			this.past.push({ state, undo });
 		}
-
 	}
 
 	undo() {
 		if (this.past.length > 0) {
 			this.isUndoing = true;
 			const temp = this.past.pop();
-			console.log(temp);
 			temp.undo(temp.state);
 			this.isUndoing = false;
 		}
