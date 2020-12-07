e1bf91c208ac9b515cdc66207fd77a2790736571
Anthony Hill <anthony.hill@d2l.com>
2020-12-07 14:48:11 -0500

    unit tests

--- test/components/active-courses-table.test.js
+++ test/components/active-courses-table.test.js
@@ -12,7 +12,7 @@ describe('d2l-insights-active-courses-table', () => {
 
 	const orgUnits = [
 		[1, 'Course 1', 3, [3, 4], true],
-		[2, 'Course 2', 3, [3, 10], true]
+		[2, 'Course 2', 3, [3, 10], false]
 	];
 
 	describe('constructor', () => {
@@ -32,6 +32,26 @@ describe('d2l-insights-active-courses-table', () => {
 		it('showPredictedGradeCol should return true', async() => {
 			const el = await fixture(html`<d2l-insights-active-courses-table .userCourses=${userCourses} .orgUnits=${orgUnits}></d2l-insights-active-courses-table>`);
 			expect(el.showPredictedGradeCol).to.deep.equal(true);
+		});
+
+		it('should render active course title and table if there are active courses', async() => {
+			const el = await fixture(html`<d2l-insights-active-courses-table .userCourses=${userCourses} .orgUnits=${orgUnits}></d2l-insights-active-courses-table>`);
+			const title = el.shadowRoot.querySelector('.d2l-heading-3');
+			const table = el.shadowRoot.querySelector('d2l-insights-table');
+			expect(title.innerText).to.deep.equal('Active Courses');
+			expect(table).to.not.be.null;
+		});
+
+		it('should not render active course title and table if there are no active courses', async() => {
+			const inactiveOrgUnits = [
+				[1, 'Course 1', 3, [3, 4], false],
+				[2, 'Course 2', 3, [3, 10], false]
+			];
+			const el = await fixture(html`<d2l-insights-active-courses-table .userCourses=${userCourses} .orgUnits=${inactiveOrgUnits}></d2l-insights-active-courses-table>`);
+			const title = el.shadowRoot.querySelector('.d2l-heading-3');
+			const table = el.shadowRoot.querySelector('d2l-insights-table');
+			expect(title).to.be.null;
+			expect(table).to.be.null;
 		});
 	});
 });
