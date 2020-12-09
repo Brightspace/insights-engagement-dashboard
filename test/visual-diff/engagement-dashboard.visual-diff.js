899a90e53891a21ebeeb983d4bf27b5fa727e20c
Nick Hallman <Nick.Hallman@D2L.com>
2020-12-09 13:59:20 -0500

    ?

---
+++ test/visual-diff/engagement-dashboard.visual-diff.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+const VisualDiff = require('@brightspace-ui/visual-diff');
+
+describe('engagement-dashboard', () => {
+
+	const visualDiff = new VisualDiff('engagement-dashboard', __dirname);
+
+	let browser, page;
+
+	before(async() => {
+		browser = await puppeteer.launch();
+		page = await visualDiff.createPage(browser);
+		await page.setViewport({
+			width: 1275,
+			height: 2200,
+			deviceScaleFactor: 1
+		});
+		await page.goto(
+			`${visualDiff.getBaseUrl()}/test/visual-diff/d2l-insights-engagement-dashboard.visual-diff.html`,
+			{ waitUntil: ['networkidle0', 'load'] }
+		);
+
+		await page.keyboard.press('Tab');
+		await page.keyboard.press('Enter');
+		await page.bringToFront();
+	});
+
+	after(() => browser.close());
+
+	it('Desktop', async function() {
+		const rect = await visualDiff.getRect(page, 'd2l-insights-engagement-dashboard');
+		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
+
+	it('Mobile', async function() {
+		await page.setViewport({
+			width: 682,
+			height: 3000,
+			deviceScaleFactor: 1
+		});
+		const rect = await visualDiff.getRect(page, 'd2l-insights-engagement-dashboard');
+		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
+
+});
