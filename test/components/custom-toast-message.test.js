221bc876d603faffae3fb5808a2e9e1ed167b959
Rohit Vinnakota <rohitvinnakota@github.com>
2020-12-17 09:58:07 -0500

    Minor changes

--- test/components/custom-toast-message.test.js
+++ test/components/custom-toast-message.test.js
@@ -21,11 +21,11 @@ describe('d2l-insights-custom-toast-message', () => {
 	describe('render', () => {
 		it('should render the invalid system last accessed value toast message as expected', async() => {
 			const el = await fixture(html`<d2l-insights-custom-toast-message></d2l-insights-custom-toast-message>`);
-			await new Promise(resolve => setTimeout(resolve, 200)); // allow fetch to run
-			expect(el.shadowRoot.querySelector('d2l-alert-toast').innerHTML).to.deep.equal('');
+			await new Promise(resolve => setTimeout(resolve, 200));
+			expect(el.shadowRoot.querySelector('d2l-alert-toast').innerHTML).to.equal('');
 			el.systemLastAccessError();
 			await new Promise(resolve => setTimeout(resolve, 200));
-			expect(el.shadowRoot.querySelector('d2l-alert-toast').innerHTML).to.deep.equal('Your settings could not be saved, please try again. System Access thresholds need to be between 1 and 30.');
+			expect(el.shadowRoot.querySelector('d2l-alert-toast').innerHTML).to.deep.equal('Your settings could not be saved. System Access thresholds need to be between 1 and 30.');
 			expect(el.shadowRoot.querySelector('d2l-alert-toast').open).to.equal(true);
 		});
 	});
