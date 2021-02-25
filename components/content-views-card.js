dd1251c0e022cef920116c3c1379531b8b6a1b07
Vitalii Misechko <Vitalii.Misechko@d2l.com>
2021-02-25 16:01:20 +0200

    Fixes for Content Views Over Time

--- components/content-views-card.js
+++ components/content-views-card.js
@@ -124,10 +124,23 @@ class ContentViewsCard extends SkeletonMixin(Localizer(MobxLitElement)) {
 				enabled: false,
 			},
 			tooltip: {
-				enabled: false
+				enabled: true,
+				backgroundColor: 'var(--d2l-color-ferrite)',
+				borderWidth: 0,
+				borderRadius: 15,
+				shadow: false,
+				padding: 10,
+
+				style: {
+					color: 'var(--d2l-color-white)',
+					fontSize: '10px',
+					fontFamily: 'Lato',
+					lineHeight: '18px'
+				}
 			},
 			xAxis: {
-				tickInterval:  7 * 24 * 3600 * 1000, //week
+				startOnTick: true,
+				endOnTick: true,
 				type: 'datetime',
 				labels: {
 					formatter: function() {
@@ -160,10 +173,8 @@ class ContentViewsCard extends SkeletonMixin(Localizer(MobxLitElement)) {
 						fontFamily: 'Lato'
 					}
 				},
-				max: 100,
-				tickPositions: [0, 25, 50, 75, 100],
-				startOnTick: true,
-				endOnTick: true,
+				max: this.isDemo ? 100 : undefined,
+				tickPositions: this.isDemo ? [0, 25, 50, 75, 100] : undefined,
 				gridLineWidth: 1,
 				gridLineColor: 'var(--d2l-color-mica)',
 				labels: {
@@ -206,6 +217,11 @@ class ContentViewsCard extends SkeletonMixin(Localizer(MobxLitElement)) {
 	get _trendData() {
 		return this.userData
 			.contentViews
+			.map(courseData => ({
+				...courseData,
+				orgUnitId: courseData.orgUnitId ? Number(courseData.orgUnitId) : 0,
+				data: courseData.data.map(point => ({ x: point[0], y: point[1] }))
+			}))
 			.filter(courseData => this._filteredOrgUnitIds.has(courseData.orgUnitId));
 	}
 
@@ -241,6 +257,9 @@ class ContentViewsCard extends SkeletonMixin(Localizer(MobxLitElement)) {
 		return this._trendData
 			.map((course) => ({
 				...course,
+				marker: {
+					enabled: course.data && course.data.length === 1
+				},
 				// It is read as `Course 1, series 1 of 3 with 8 data points.`
 				name: this._orgUnitName(course.orgUnitId),
 				color: selected(course) ? colors[this._userOrgUnitIds.findIndex(orgId => orgId === course.orgUnitId)] : 'var(--d2l-color-mica)' }));
