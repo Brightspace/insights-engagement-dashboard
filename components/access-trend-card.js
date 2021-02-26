d8683bfdbe59bf1695bb25d3395397dd595e9abc
Vitalii Misechko <Vitalii.Misechko@d2l.com>
2021-02-26 13:59:54 +0200

    Bring upfront an active course for trand charts

--- components/access-trend-card.js
+++ components/access-trend-card.js
@@ -308,7 +308,9 @@ class AccessTrendCard extends SkeletonMixin(Localizer(MobxLitElement)) {
 				// It is read as `Course 1, series 1 of 3 with 8 data points.`
 				name: this._orgUnitName(course.orgUnitId),
 				lineColor:  'var(--d2l-color-white)',
-				color: selected(course) ? colors[this._userOrgUnitIds.findIndex(orgId => orgId === course.orgUnitId)] : 'var(--d2l-color-mica)' }));
+				color: selected(course) ? colors[this._userOrgUnitIds.findIndex(orgId => orgId === course.orgUnitId)] : 'var(--d2l-color-mica)',
+				zIndex: selected(course) ? 1 : undefined
+			}));
 	}
 }
 decorate(AccessTrendCard, {
