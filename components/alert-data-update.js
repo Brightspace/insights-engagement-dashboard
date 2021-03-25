1615cd35974e1d93f0b503b2c944f20c6eb19d4e
Nick Hallman <Nick.Hallman@D2L.com>
2021-03-25 14:46:02 -0400

    remove console log

--- components/alert-data-update.js
+++ components/alert-data-update.js
@@ -10,7 +10,6 @@ import { SkeletonMixin } from '@brightspace-ui/core/components/skeleton/skeleton
 class FilterEvent {
 
 	constructor(message, description) {
-		console.log(description);
 		this._message = message;
 		this._description = description;
 		this._isRendered = false;
