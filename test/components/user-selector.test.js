a9d93da8136d50a849cab613cd174480654d0d3b
Vitalii Misechko <Vitalii.Misechko@d2l.com>
2021-04-22 19:57:41 +0300

    Add basic tests for user-selector

---
+++ test/components/user-selector.test.js
@@ -0,0 +1,23 @@
+import '../../components/user-selector';
+import { expect, fixture, html } from '@open-wc/testing';
+import { runConstructor } from '@brightspace-ui/core/tools/constructor-test-helper.js';
+
+describe.only('d2l-insights-user-selector', () => {
+
+	describe('constructor', () => {
+		it('should construct', () => {
+			runConstructor('d2l-insights-user-selector');
+		});
+	});
+
+	describe('accessibility', () => {
+		it('should pass all axe tests', async() => {
+			const el = await fixture(html`<d2l-insights-user-selector demo></d2l-insights-user-selector>`);
+			// give it a second to make sure inner table and paging controls load in
+			// await new Promise(resolve => setTimeout(resolve, 200));
+			await el.updateComplete;
+
+			await expect(el).to.be.accessible();
+		});
+	});
+});
