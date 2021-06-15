60c81f6eaf0217c2b9395d09388c400e1dbc31f8
Nick Hallman <Nick.Hallman@D2L.com>
2021-06-15 08:56:16 -0400

    use slack webhook secret

--- .github/notify.sh
+++ .github/notify.sh
@@ -1,2 +1,2 @@
 #!bin/bash
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Translation Push Created"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Translation PR Created"}' ${WEBHOOK}
