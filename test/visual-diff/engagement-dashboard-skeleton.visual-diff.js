62c677d164683aea2decc701524890e3cbcf00f7
Oleksandr Pets <pec.alex@gmail.com>
2021-06-22 13:01:01 +0300

    US128971: fix vis-diff

--- test/visual-diff/engagement-dashboard-skeleton.visual-diff.js
+++ test/visual-diff/engagement-dashboard-skeleton.visual-diff.js
@@ -25,6 +25,7 @@ describe('engagement-dashboard-skeleton', () => {
 			`${visualDiff.getBaseUrl()}/test/visual-diff/d2l-insights-engagement-dashboard.visual-diff.html#delay=3000`,
 			{ waitUntil: ['networkidle0', 'load'] }
 		);
+		await new Promise(res => setTimeout(res, 500));
 		await page.bringToFront();
 	});
 
